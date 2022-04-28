9ec6d7f1ffc3965c66a5965b67f106a212a0c742
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-28 12:06:08 -0600

    gaem card

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -2,15 +2,21 @@
   <div class="card game-card">
     <div class="image-wrapper">
       <!-- Image Goes Here -->
+      <img :src="image" :alt="name">
     </div>
     <div class="info-wrapper flex-col">
       <!-- Game Name Goes Here -->
+      {{name}}
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'GameCard'
+    name: 'GameCard', 
+    props: {
+      name: String, 
+      image: String, 
+    }, 
   }
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,14 +2,20 @@
   <div>
     <div class="search">
       <!-- Search Form Goes Here -->
+      <form @submit="getSearchResults">
+        <input type="text" :value="searchQuery" @input="handleChange">
+        <button>Search</button>
+      </form>
       <h2>Search Results</h2>
-      <section class="search-results container-grid"></section>
+      <section  v-if="searched" class="search-results container-grid">
+        <GameCard :key="result.id" :gameId="result.id" v-for="result in searchResults" :image="result.background_image" :name="result.name" @click="selectGame(result.id)"/>
+      </section>
     </div>
 
-    <div class="genres">
+    <div v-if="!searched" class="genres">
       <h2>Genres</h2>
-      <section class="container-grid">
-        <GenreCard :key="genre.id" v-for="genre in genres" :image="genre.image_background" :name="genre.name"  />
+      <section class="container-grid" >
+        <GenreCard :key="genre.id" v-for="genre in genres" :image="genre.image_background" :name="genre.name"/>
       </section>
     </div>
   </div>
@@ -18,6 +24,7 @@
 <script>
   import axios from 'axios'
   import GenreCard from '../components/GenreCard.vue'
+  import GameCard from '../components/GameCard.vue'
   const API_KEY = process.env.VUE_APP_RAWG_KEY
 
   export default {
@@ -29,7 +36,8 @@
       searched: false
     }),
     components: {
-      GenreCard
+      GenreCard, 
+      GameCard
     },
     mounted() {
       this.getGenres()
@@ -42,11 +50,17 @@
       },
       async getSearchResults(e) {
         e.preventDefault()
+        const res = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&search=${this.searchQuery}`)
+        console.log(res)
+        this.searchResults = res.data.results
+        this.searched = true
       },
       handleChange(event) {
+        this.searchQuery = event.target.value
         console.log(event)
       },
       selectGame(gameId) {
+        this.$router.push(`/details/${gameId}`)
         console.log(gameId)
       }
     }
