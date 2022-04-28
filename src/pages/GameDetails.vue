0e1612cb4ce7892ba65bfd52964aeeb593b53215
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-28 12:18:45 -0600

    8

--- src/pages/GameDetails.vue
+++ src/pages/GameDetails.vue
@@ -1,27 +1,41 @@
 <template>
-  <div class="game-content">
+  <div v-if="gameDetails" class="game-content">
     <section class="image-container">
-      <div></div>
+      <div>
+        <img :src="gameDetails.background_image" :alt="gameDetails.name">
+      </div>
     </section>
     <section class="details">
       <div class="flex-row space"></div>
       <div>
-        <h3></h3>
+        <h3>{{gameDetails.name}}</h3>
+      </div>
+      <div>
+        <p>{{gameDetails.description_raw}}</p>
       </div>
     </section>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  const API_KEY = process.env.VUE_APP_RAWG_KEY
+
   export default {
     name: 'GameDetails',
     data: () => ({
       gameDetails: null
     }),
-    mounted() {},
+    mounted() {
+      this.getGameDetails()
+    },
     methods: {
       async getGameDetails() {
         // Get game id from router here
+        const gameId = this.$route.params.game_id
+        const res = await axios.get(`https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`)
+        console.log(res)
+        this.gameDetails = res.data
       }
     }
   }
